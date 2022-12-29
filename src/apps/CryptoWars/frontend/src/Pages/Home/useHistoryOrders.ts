c111027dc9907b75bd18eb91a3e961e46588d1cf
Javier Vazquez Fernandez <javier@MacBook-Pro-de-Javier-2.local>
2022-12-29 17:52:44 +0100

    Fix frontend dependencies

--- src/apps/CryptoWars/frontend/src/Pages/Home/useHistoryOrders.ts
+++ src/apps/CryptoWars/frontend/src/Pages/Home/useHistoryOrders.ts
@@ -1,7 +1,26 @@
 import { useQuery } from 'react-query';
 import { handleQueryResult, QueryTrigger } from '../../API/query';
 import axios from 'axios';
-import { Trades } from '../../../../backend/server';
+
+export type Trades = Array<Trade>;
+
+export interface Trade {
+  symbol: string;
+  size: number;
+  orderId: string;
+  fee: number;
+  state: string; // filled
+  side: string; // close_short
+  marginCoin: string; // USDT
+  filledAmount: number;
+  orderType: string; // limit
+  leverage: string;
+  marginMode: string; // crossed
+  cTime: string; // unix timestamp
+  uTime: string; // unix timestamp
+  totalProfits: number;
+  posSide: string; // short / long
+}
 
 interface HistoryOrdersResponse {
   orders: Trades;
